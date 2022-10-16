d95aaa8bb0b1375a8ed63f82cd036dc2ac62e4cc
tonyandy5630 <tonyandy456@gmail.com>
2022-10-16 12:41:08 +0700

    add fetch post

--- components/body-feed/MainFeed.js
+++ components/body-feed/MainFeed.js
@@ -3,11 +3,39 @@ import CreatePost from "./create-post/CreatePost";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../recipe-post/recipe-post-minimize/RecipePostMiniminze";
 import { useRouter } from "next/router";
+import axios from "axios";
+import { useEffect } from "react";
 
 const MainFeed = () => {
   const router = useRouter();
+
+  useEffect(() => {
+    FetchPost();
+  }, []);
+
   const { category } = router.query;
   console.log(category);
+
+  function FetchPost() {
+    var config = {
+      method: "get",
+      url: "http://api.bakarya.com/api/recipes",
+      headers: {},
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  useEffect(() => {
+    FetchPost();
+  }, []);
+
   return (
     <Col xl={6} fluid='true'>
       <Container
@@ -17,7 +45,6 @@ const MainFeed = () => {
         }}
       >
         <CreatePost />
-        <Post />
         <Post />
 
         {/* <InfiniteScroll
