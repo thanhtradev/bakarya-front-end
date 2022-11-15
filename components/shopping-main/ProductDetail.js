caf0f784e3549f0655120d55aa91a1dbd484b332
RedTranFED <d.fed27@gsoft01.tk>
2022-11-15 20:19:01 +0700

    add checkout

--- components/shopping-main/ProductDetail.js
+++ components/shopping-main/ProductDetail.js
@@ -28,34 +28,34 @@ const ProductDetail = ({ id, img, name, quantity, price, description }) => {
       setTestComment((prev) =>
         prev.concat(
           <Stack
-            justifyContent='flex-start'
-            alignItems='center'
-            direction='row'
+            justifyContent="flex-start"
+            alignItems="center"
+            direction="row"
             spacing={0.8}
-            paddingX='10px'
+            paddingX="10px"
             key={Math.random() * 100}
           >
-            <Avatar variant='circular' sx={{ width: "35px", height: "35px" }} />
-            <Stack alignItems='flex-start'>
+            <Avatar variant="circular" sx={{ width: "35px", height: "35px" }} />
+            <Stack alignItems="flex-start">
               <Paper
                 elevation={1}
                 component={Stack}
-                direction='row'
-                alignItems='flex-start'
+                direction="row"
+                alignItems="flex-start"
                 spacing={2}
                 justifyContent={"center"}
-                paddingX='10px'
+                paddingX="10px"
                 sx={{ width: "1", bgcolor: "#e6e6e6" }}
               >
                 <Stack
-                  justifyContent='space-evenly'
-                  alignItems='flex-start'
+                  justifyContent="space-evenly"
+                  alignItems="flex-start"
                   sx={{ width: "fit-content" }}
                 >
-                  <Typography variant='body1' fontWeight='bold' fontSize='13px'>
+                  <Typography variant="body1" fontWeight="bold" fontSize="13px">
                     {`test`}
                   </Typography>
-                  <Typography variant='body1' fontSize='15px'>
+                  <Typography variant="body1" fontSize="15px">
                     {`test`}
                   </Typography>
                 </Stack>
@@ -74,46 +74,46 @@ const ProductDetail = ({ id, img, name, quantity, price, description }) => {
   return (
     <Card
       component={Stack}
-      direction='row'
+      direction="row"
       sx={{ height: "30rem", width: "50rem", padding: "10px" }}
       spacing={2}
     >
       <CardMedia
-        component='img'
-        height='100%'
-        image='https://images.unsplash.com/photo-1667213768683-278bcfce53cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80'
-        alt='green iguana'
+        component="img"
+        height="100%"
+        image="https://images.unsplash.com/photo-1667213768683-278bcfce53cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80"
+        alt="green iguana"
         sx={{ backgroundSize: "cover", width: "0.5", borderRadius: "10px" }}
       />
       <CardContent
         component={Stack}
-        alignContent='center'
-        justifyContent='space-between'
+        alignContent="center"
+        justifyContent="space-between"
         sx={{ width: "0.5" }}
       >
         <Stack
-          direction='row'
-          justifyContent='space-between'
-          alignItems='center'
-          width='100%'
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          width="100%"
         >
           <Stack
-            component='span'
-            alignItems='flex-start'
-            maxWidth='0.85'
+            component="span"
+            alignItems="flex-start"
+            maxWidth="0.85"
             spacing={1.2}
           >
-            <Typography variant='h6' fontWeight='bold' height='100%'>
+            <Typography variant="h6" fontWeight="bold" height="100%">
               {name}
             </Typography>
           </Stack>
-          <Stack component='span' alignItems='flex-start'>
+          <Stack component="span" alignItems="flex-start">
             <Typography
-              variant='h5'
-              fontWeight='bold'
-              height='100%'
-              textAlign='center'
-              padding='0'
+              variant="h5"
+              fontWeight="bold"
+              height="100%"
+              textAlign="center"
+              padding="0"
             >
               {`${price} $`}
             </Typography>
@@ -131,10 +131,10 @@ const ProductDetail = ({ id, img, name, quantity, price, description }) => {
           </Stack>
         </Box>
         <Stack
-          direction='row'
-          width='100%'
-          justifyContent='space-between'
-          height='2.5rem'
+          direction="row"
+          width="100%"
+          justifyContent="space-between"
+          height="2.5rem"
         >
           <ProductQuantity
             maxQuantity={quantity}
@@ -142,7 +142,7 @@ const ProductDetail = ({ id, img, name, quantity, price, description }) => {
             price={price}
           />
           <Button
-            variant='contained'
+            variant="contained"
             sx={{
               fontSize: "11px",
               height: "2.33rem",
---
+++ pages/payment/index.js
@@ -0,0 +1,374 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import styles from "./Payment.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { payment } from "../../redux/user.reducer";
+function Payment() {
+  const dispatch = useDispatch();
+  const userSlice = useSelector((state) => state.userSlice.pay);
+  const [checkOne, setCheckOne] = useState(false);
+  const payments = () => {
+    dispatch(payment());
+  };
+  return (
+    <div className={styles["view-wrapper"]}>
+      {/*Wrapper*/}
+      <div className={styles["shop-wrapper"]}>
+        {/*Header*/}
+        <div className={`${styles["cart-container"]} ${styles["is-payment"]}`}>
+          <div className={styles["cart-header"]}>
+            <div className={styles["header-inner"]}>
+              <h2 id="checkout-step-title">Payment</h2>
+              <div className={styles["header-actions"]}>
+                <div className={styles["buttons"]}>
+                  <a
+                    href="/navbar-v1-ecommerce-checkout.html"
+                    className={`${styles["button is-light"]} ${styles["is-hidden"]}`}
+                  >
+                    <span className={`${styles["icon"]} ${styles["is-small"]}`}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className={`${styles["feather"]} ${styles["feather-arrow-left"]}`}
+                      >
+                        <line x1={19} y1={12} x2={5} y2={12} />
+                        <polyline points="12 19 5 12 12 5" />
+                      </svg>
+                    </span>
+                    <span>Back</span>
+                  </a>
+                  <Link
+                    href="/checkout"
+                    className={`${styles["button"]} ${styles["is-solid"]} ${styles["primary-button"]} ${styles["raised"]}`}
+                  >
+                    View Order
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/*Container*/}
+        <div
+          id="payment-container"
+          className={`${styles["checkout-container"]} ${
+            checkOne && styles["is-hidden"]
+          }`}
+        >
+          {/*Left Side*/}
+          <div className={styles["left"]}>
+            <div className={styles["left-header"]}>
+              <div className={styles["header-inner"]}>
+                <img
+                  className={styles["avatar"]}
+                  src="assets/img/icons/logos/store.svg"
+                  alt="image"
+                />
+                <div className={styles["separator"]} />
+                <div className={styles["title-wrap"]}>
+                  <h3 className={styles["payment-title"]}>Pay your order</h3>
+                  <p className={styles["payment-subtitle"]}>
+                    Enter your credit card information
+                  </p>
+                </div>
+              </div>
+              <div className={styles["header-coupon"]}>
+                <div className={`${styles["field"]} ${styles["is-grouped"]}`}>
+                  <div className={styles["control"]}>
+                    <input
+                      type="text"
+                      className={styles["input"]}
+                      placeholder="Gift card or discount code"
+                    />
+                  </div>
+                  <div className={styles["control"]}>
+                    <button
+                      className={`${styles["button"]} ${styles["is-solid"]} ${styles["primary-button"]} ${styles["raised"]}`}
+                    >
+                      Apply
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className={styles["header-foot"]}>
+                <span className={styles["label"]}>Total</span>
+                <span className={styles["amount"]} data-currency="USD">
+                  ${userSlice[0].sum}
+                </span>
+              </div>
+            </div>
+            <div className={styles["left-body"]} />
+          </div>
+          {/*Right Side*/}
+          <div className={styles["right"]}>
+            <div className={styles["payment-form"]}>
+              <form
+                action="/charge"
+                method="post"
+                id="stripe-payment-form"
+                className={`${styles["provider-form"]} ${styles["is-active"]}`}
+              >
+                <div
+                  id="checkout-contact-section"
+                  className={`${styles["form-section"]} ${styles["has-margin-bottom"]}`}
+                >
+                  <div className={styles["form-section-header"]}>
+                    <h3>Contact Information</h3>
+                  </div>
+                  <div className={styles["field"]}>
+                    <label htmlFor="card-element">Email Address</label>
+                    <div className={styles["control"]}>
+                      <input
+                        className={styles["input"]}
+                        placeholder="Enter your email address"
+                        name="checkout_email"
+                        autofocus
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div
+                  id="checkout-payment-section"
+                  className={styles["form-section"]}
+                >
+                  <div className={styles["form-section-header"]}>
+                    <h3>Payment Information</h3>
+                  </div>
+                  <div className={styles["field"]}>
+                    <label htmlFor="card-element">Name on card</label>
+                    <div className={styles["control"]}>
+                      <input
+                        className={styles["input"]}
+                        placeholder="Enter the name on the credit card"
+                        name="checkout_name"
+                      />
+                    </div>
+                  </div>
+                  <div className={styles["field"]}>
+                    <label htmlFor="card-element">Credit or debit card</label>
+                    <div className={styles["control"]}>
+                      <div
+                        id="card-element"
+                        className={`${styles["StripeElement"]} ${styles["StripeElement--empty"]}`}
+                      >
+                        <div
+                          className={styles["__PrivateStripeElement"]}
+                          style={{
+                            margin: "0px !important",
+                            padding: "0px !important",
+                            border: "none !important",
+                            display: "block !important",
+                            background: "transparent !important",
+                            position: "relative !important",
+                            opacity: "1 !important",
+                          }}
+                        >
+                          <iframe
+                            frameBorder={0}
+                            allowTransparency="true"
+                            scrolling="no"
+                            role="presentation"
+                            allow="payment *"
+                            src="https://js.stripe.com/v3/elements-inner-card-3f1dc6dccf75fce268f106acf1b9cf98.html#wait=false&mids[guid]=NA&mids[muid]=NA&mids[sid]=NA&style[base][fontSize]=14px&style[base][color]=%23595d6e&rtl=false&componentName=card&keyMode=test&apiKey=pk_test_6pRNASCoBOKtIshFeQd4XMUh&referrer=http%3A%2F%2Flocalhost%3A3000%2Fnavbar-v1-ecommerce-payment.html&controllerId=__privateStripeController7811"
+                            title="Secure card payment input frame"
+                            style={{
+                              border: "none !important",
+                              margin: "0px !important",
+                              padding: "0px !important",
+                              width: "1px !important",
+                              minWidth: "100% !important",
+                              overflow: "hidden !important",
+                              display: "block !important",
+                              userSelect: "none !important",
+                              transform: "translate(0px) !important",
+                              colorScheme: "light only !important",
+                              height: "16.8px",
+                            }}
+                          />
+                          {/* <input
+                            className={styles["__PrivateStripeElement-input"]}
+                            aria-hidden="true"
+                            aria-label=" "
+                            autoComplete="false"
+                            maxLength={1}
+                            style={{
+                              border: "none !important",
+                              display: "block !important",
+                              position: "absolute !important",
+                              height: "1px !important",
+                              top: "-1px !important",
+                              left: "0px !important",
+                              padding: "0px !important",
+                              margin: "0px !important",
+                              width: "100% !important",
+                              opacity: "0 !important",
+                              background: "transparent !important",
+                              pointerEvents: "none !important",
+                              fontSize: "16px !important",
+                            }}
+                          /> */}
+                        </div>
+                      </div>
+                      {/* Used to display Element errors */}
+                      <div id="card-errors" role="alert" />
+                    </div>
+                  </div>
+                  <div className={styles["field"]}>
+                    <label>Country</label>
+                    <div
+                      className={`${styles["control"]} ${styles["has-icons-left"]}`}
+                    >
+                      <div className={styles["select"]}>
+                        <select>
+                          <option />
+                          <option selected>United States of America</option>
+                          <option>Belgium</option>
+                          <option>Brazil</option>
+                          <option>Canada</option>
+                          <option>France</option>
+                          <option>Germany</option>
+                          <option>Mexico</option>
+                          <option>Spain</option>
+                          <option>England</option>
+                          <option>Luxembourg</option>
+                        </select>
+                      </div>
+                      <div
+                        className={`${styles["icon"]} ${styles["is-small"]} ${styles["is-left"]}`}
+                      >
+                        <i
+                          className={`${styles["mdi"]} ${styles["mdi-earth"]}`}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={`${styles["field"]} ${styles["is-button"]}`}>
+                  <div
+                    className={`${styles["buttons"]} ${styles["has-addons"]}`}
+                  >
+                    <button
+                      id="payment-button"
+                      type="button"
+                      className={`${styles["button"]} ${styles["is-solid"]} ${styles["primary-button"]}`}
+                      onClick={() => {
+                        setCheckOne(true);
+                        payments();
+                      }}
+                    >
+                      Confirm Payment
+                    </button>
+                    <button
+                      type="button"
+                      className={`${styles["button"]} ${styles["is-solid"]} ${styles["primary-button"]}`}
+                      style={{ borderRadius: " 0px 10px 10px 0px" }}
+                    >
+                      ${userSlice[0].sum}
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div
+          id="confirmation-container"
+          className={`${styles["checkout-container"]} ${
+            !checkOne && styles["is-hidden"]
+          }`}
+        >
+          <div className={styles["confirmation-box"]}>
+            <svg
+              id="successAnimation"
+              className={styles["animated"]}
+              xmlns="http://www.w3.org/2000/svg"
+              width={70}
+              height={70}
+              viewBox="0 0 70 70"
+            >
+              <path
+                className={styles["successAnimationResult"]}
+                fill="#41d6c3"
+                d="M35,60 C21.1928813,60 10,48.8071187 10,35 C10,21.1928813 21.1928813,10 35,10 C48.8071187,10 60,21.1928813 60,35 C60,48.8071187 48.8071187,60 35,60 Z M23.6332378,33.2260427 L22.3667622,34.7739573 L34.1433655,44.40936 L47.776114,27.6305926 L46.223886,26.3694074 L33.8566345,41.59064 L23.6332378,33.2260427 Z"
+              />
+              <circle
+                className={styles["successAnimationCircle"]}
+                cx={35}
+                cy={35}
+                r={24}
+                stroke="#979797"
+                strokeWidth={2}
+                strokeLinecap="round"
+                fill="transparent"
+              />
+              <polyline
+                id="successAnimationCheck"
+                stroke="#979797"
+                strokeWidth={2}
+                points="23 34 34 43 47 27"
+                fill="transparent"
+              />
+            </svg>
+            <h3>Your payment was successful.</h3>
+            <p>
+              Thank you for your purchase. We will immediatly start preparing
+              your order and ship it to you.
+            </p>
+            <div className={styles["order-summary"]}>
+              <h4>Order Summary</h4>
+              <div className={styles["order-line"]}>
+                <div className={styles["item"]}>
+                  <span>Transaction ID</span>
+                </div>
+                <div className={styles["amount"]}>
+                  <span>#45894</span>
+                </div>
+              </div>
+              <div className={styles["order-line"]}>
+                <div className={styles["item"]}>
+                  <span>Subtotal</span>
+                </div>
+                <div className={styles["amount"]}>
+                  <span data-currency="USD">${userSlice[0].sum}</span>
+                </div>
+              </div>
+              <div className={styles["order-line"]}>
+                <div className={styles["item"]}>
+                  <span>Shipping</span>
+                </div>
+                <div className={styles["amount"]}>
+                  <span data-currency="USD">$3</span>
+                </div>
+              </div>
+              <div className={styles["order-line"]}>
+                <div className={styles["item"]}>
+                  <span>Taxes</span>
+                </div>
+                <div className={styles["amount"]}>
+                  <span data-currency="USD">$2</span>
+                </div>
+              </div>
+              <div className={styles["order-line"]}>
+                <div className={`${styles["item"]} ${styles["is-total"]}`}>
+                  <span>Total</span>
+                </div>
+                <div className={`${styles["amount"]} ${styles["is-total"]}`}>
+                  <span data-currency="USD">${userSlice[0].sum + 3 + 5}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Payment;
